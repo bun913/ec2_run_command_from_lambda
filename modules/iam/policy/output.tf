9a4c04d48bc505783d18482ead0331e8820af404
bun913 <imanau03@gmail.com>
2021-10-31 07:18:59 +0900

    Add: iam_roleの作成を追加

--- modules/iam/policy/output.tf
+++ modules/iam/policy/output.tf
@@ -1,7 +1,3 @@
-output "policy_id" {
-  value = aws_iam_policy.template_file.id
-}
-
-output "policy_arn" {
-  value = aws_iam_policy.template_file.arn
+output "aws_iam_role" {
+  value = aws_iam_policy.policy
 }
